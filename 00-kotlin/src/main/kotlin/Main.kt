c85809170c72406f47cc69f7a7269ff2ed1865a6
Jonathan Tejada <jonathan.tejada@epn.edu.ec>
2021-06-08 13:02:47 -0500

    00-kodlin
    Primera clase-variables

---
+++ 00-kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+fun main (){
+    println("hola mundo")
+    //java
+    //tipo nombre= valor
+    //koklim
+    var edadProfesor =32
+    //var edadProfesor : Int = 32
+    var sueldoProfesor = 1.23
+    edadProfesor =25
+
+    //Vatiables mutables e inmutables
+
+    //mutables (se pueden reasignar)
+
+    var edadCachorro = 0
+    edadCachorro = 1
+    edadCachorro = 2
+
+    //inmutables (no se pueden reasignar)
+
+    val numeroCedula= 321564864
+   //numeroCedula = 12
+
+    //Tipos de variales
+
+    val nombreProfesor: String = "Adrian"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char ='s'
+    val fechaNacimiento: Date = Date();
+}
